b35b7095bfe95530a282cba4ff057a70986e1786
Jake Vigeant <jmv43@duke.edu>
2022-09-16 17:31:03 -0400

    Initialized functional components

---
+++ frontend/src/components/Leaderboard.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const d = () => {
+  return (
+    <div>d</div>
+  )
+}
---
+++ frontend/src/components/TutorProfile.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const TutorProfile = () => {
+  return (
+    <div>TutorProfile</div>
+  )
+}
