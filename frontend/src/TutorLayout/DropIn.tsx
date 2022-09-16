517855f312973bc24f0f89138307c0e9a4ed714d
Savar Jain - Home MBA <jainsavar@gmail.com>
2022-09-16 17:17:31 -0400

    merge conflict

--- frontend/src/TutorLayout/DropIn.tsx
+++ frontend/src/TutorLayout/DropIn.tsx
@@ -1,18 +1,17 @@
-import React, {useState} from 'react';
-import Fab from '@mui/material/Fab';
-import AddIcon from '@mui/icons-material/Add';
-
+import React, { useState } from "react";
+import Fab from "@mui/material/Fab";
+// import AddIcon from '@mui/icons-material/Add';
 
 function DropIn() {
   const [available, setAvailable] = useState(false);
   return (
     <>
-      {available} && <Fab color="primary" aria-label="add">
-  <AddIcon />
-</Fab>
-
+      {available} &&{" "}
+      <Fab color="primary" aria-label="add">
+        {/* <AddIcon /> */}
+      </Fab>
     </>
-  )
+  );
 }
 
 export default DropIn;
