53ccf0874e66bb616d8852adabb2c43bdf4faa70
Savar Jain - Home MBA <jainsavar@gmail.com>
2022-09-17 04:08:26 -0400

    Fix bugs

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,10 +2,9 @@ import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/auth/login/Login";
-import Dashboard from "./pages/tutorDashboard";
 import SignUp from "./pages/auth/register/SignUp";
 import { StudentDashboard } from "./pages/studentDashboard/StudentDashboard";
-import { TutorDashboard } from "./pages/tutorDashboard/TutorDashboard";
+import Dashboard from "./pages/tutorDashboard";
 import TutorDropIn from "./pages/tutorDashboard/TutorDropIn.jsx";
 import Profile from "./pages/profile";
 import Tutor_Feed from "./pages/studentDashboard/Tutor_Feed";
@@ -17,9 +16,8 @@ function App() {
         <Route path="/" element={<Login />} />
         <Route path="/auth/signup" element={<SignUp />} />
         <Route path="/studentdashboard" element={<StudentDashboard />} />
-        <Route path="/tutordashboard" element={<TutorDashboard />} />
+        <Route path="/tutordashboard" element={<Dashboard />} />
         <Route path="/tutorfeed" element={<Tutor_Feed />} />
-        <Route path="/tutorprofile" element={<TutorDashboard />} />
         <Route path="/tutordropin" element={<TutorDropIn />} />
         <Route path="/profile" element={<Profile />} />
       </Routes>
