9078d93866002383acde2b89864ce0edc444e9df
Nethaniel Berhane <natu2002@gmail.com>
2022-09-16 21:47:29 -0400

    Authorize User Login

--- backend/app.py
+++ backend/app.py
@@ -1,3 +1,4 @@
+import bcrypt
 import os
 from dotenv import load_dotenv
 from twilio.rest import Client
@@ -7,7 +8,7 @@ from twilio.base.exceptions import TwilioRestException
 
 load_dotenv()
 app = Flask(__name__)
-app.secret_key = ''
+app.secret_key = 'testAuth'
 app.config.from_object('settings')
 
 TWILIO_ACCOUNT_SID = os.environ.get('TWILIO_ACCOUNT_SID')
@@ -17,3 +18,51 @@ VERIFY_SERVICE_SID= os.environ.get('VERIFY_SERVICE_SID')
 client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
 
 KNOWN_PARTICIPANTS = app.config['KNOWN_PARTICIPANTS']
+
+
+@app.route('/', methods=['GET', 'POST'])
+
+def login():
+    error = None
+    if request.method == 'POST':
+        username = request.form['username']
+        if username in KNOWN_PARTICIPANTS:
+            session['username'] = username
+            send_verification(username)
+            return redirect(url_for('verify_passcode_input'))
+        error = "User not found. Please try again."
+        return render_template('index.html', error = error)
+    return render_template('index.html')
+
+
+@app.route('/', methods=['GET', 'POST'])
+# ...
+def send_verification(username):
+    phone = KNOWN_PARTICIPANTS.get(username)
+    client.verify \
+        .services(VERIFY_SERVICE_SID) \
+        .verifications \
+        .create(to=phone, channel='sms')
+
+
+@app.route('/verifyme', methods=['GET', 'POST'])
+def verify_passcode_input():
+    username = session['username']
+    phone = KNOWN_PARTICIPANTS.get(username)
+    error = None
+    if request.method == 'POST':
+        verification_code = request.form['verificationcode']
+        if check_verification_token(phonenumber, verification_code):
+            return render_template('success.html', username = username)
+        else:
+            error = "Invalid verification code. Please try again."
+            return render_template('verifypage.html', error = error)
+    return render_template('verifypage.html', username = username)
+
+
+def check_verification_token(phone, token):
+    check = client.verify \
+        .services(VERIFY_SERVICE_SID) \
+        .verification_checks \
+        .create(to=phone, code=token)    
+    return check.status == 'approved'
