16e9e31b78d2868d0213d63fa095a6d5507ad1de
Savar Jain - Home MBA <jainsavar@gmail.com>
2022-09-17 11:28:15 -0400

    Delete backend 1 and 3

--- backend3/settings.py
+++
@@ -1,6 +0,0 @@
-KNOWN_PARTICIPANTS = {
-  'herooftime@hyrule.com':  '<YOUR_PHONE_NUMBER> ',
-  'zelda@hyrule.com': '+15552211986',
-  'tetra@hyrule.com': '+15553242003',
-  'akhil.reddy@rutgers.edu': '+17326667264'
-}
