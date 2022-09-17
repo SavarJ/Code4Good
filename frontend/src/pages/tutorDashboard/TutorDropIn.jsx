b58a24d612c80761b746a5391f3e813e82d9f9d4
Daniel Kogan <54873722+daminals@users.noreply.github.com>
2022-09-17 09:41:31 -0400

    football styling of tutordropin

--- frontend/src/pages/tutorDashboard/TutorDropIn.jsx
+++ frontend/src/pages/tutorDashboard/TutorDropIn.jsx
@@ -1,33 +1,55 @@
 import React, { useState } from "react";
 import Button from "@mui/material/Button";
+import Alert from '@mui/material/Alert';
+import Box from '@mui/material/Box';
+import SportsFootballIcon from '@mui/icons-material/SportsFootball';
+import "./tutordropin.css";
+
 import axios from "axios";
 
-// import AddIcon from '@mui/icons-material/Add';
-
-function DropIn() {
+function DropIn({ }) {
   // this will be the entire drop in menu component
   // default state is clocked out
   const [available, setAvailable] = useState(false);
   function clockIn() {
     const email = window.localStorage.getItem('useremail');
     // send a database request to user profile
-    axios.post('/clockin', {'email': email}) 
+    axios.post('/clockin', { 'email': email })
     setAvailable(true);
   }
   function clockOut() {
     const email = window.localStorage.getItem('useremail');
     // send a database request to user profile
-    axios.post('/clockout', {'email': email}) 
+    axios.post('/clockout', { 'email': email })
     setAvailable(false);
   }
   return (
-    <>
-      {/* if clocked in, display clock out menu */}
-      {available && <Button onClick={() => clockOut()} color="error" variant="contained">Clock out</Button> }
-      {/* if clocked out, display clock in menu */}
-      {!available && <Button onClick={() => clockIn()} color="success" variant="contained">Clock In</Button> }
-    </>
+    <Box sx={{ width: '35%' }}>
+      <SportsFootballIcon sx={{fontSize: '250%'}} className="footballIcon" />
+      <Box sx={{ border: 1, borderColor: '#12dcb6', backgroundColor: '#EDF3F2', borderRadius: 10, padding: 3 }} >
+        <ClockButton available={available} clockIn={clockIn} clockOut={clockOut} />
+      </Box>
+    </Box>
   );
+}
+
+function ClockButton({ available, clockOut, clockIn }) {
+  if (available) {
+    /* if clocked in, display clock out menu */
+    return (
+      <>
+        <Alert sx={{ 'm': 2 }} severity="success"> You are available! Please wait for a student to drop in! </Alert>
+        <Button fullWidth onClick={() => clockOut()} color="error" variant="contained">Clock out</Button>
+      </>
+    )
+  } else {
+    /* if clocked out, display clock in menu */
+    return (
+      <>
+        <Button fullWidth onClick={() => clockIn()} color="success" variant="contained">Clock In</Button>
+      </>
+    )
+  }
 }
 
 export default DropIn;
