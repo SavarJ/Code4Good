aa45cf827fa837a69f2a56fa4a2f46a00dd4d86f
mhandlerSWE <max.handler@rutgers.edu>
2022-09-17 01:04:42 -0400

    tutor feed functionality and ui improvements

--- swift/code4goodSwift/code4goodSwift/Data/testData.swift
+++ swift/code4goodSwift/code4goodSwift/Data/testData.swift
@@ -9,6 +9,7 @@ import Foundation
 
 struct TestData {
     static var tutors: [CreateTutor] = [
-        CreateTutor(id: 1, chapter: "Los Angeles, California", firstName: "Trent", lastName: "Arnold", birthday: "Thursday", bio: "Been tutoring for 5 years", zoomLink: "tutorroom1.com", availableForDropIn: true, totalHours: 10, points: 50.0, profilePic: "tutor1")
+        CreateTutor(id: 0, chapter: "Los Angeles, California", firstName: "Trent", lastName: "Arnold", birthday: "Thursday", bio: "Been tutoring for 5 years", zoomLink: "tutorroom1.com", availableForDropIn: true, totalHours: 10, points: 50.0, profilePic: "tutor1"),
+        CreateTutor(id: 1, chapter: "New York City, New York", firstName: "Serena", lastName: "Silver", birthday: "Friday", bio: "Been tutoring for 10 years. I enjoy playing soccer and snowboarding!", zoomLink: "tutorroom2.com", availableForDropIn: true, totalHours: 30, points: 150.0, profilePic: "tutor2")
         ]
 }
