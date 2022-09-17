7ad45ffd4fba50bbdfc1da49fd77e4a4a5576f99
muhaiminsarker <sarker.muhaimin15@gmail.com>
2022-09-17 04:02:10 -0400

    LOL

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,16 +7,23 @@ import SignUp from "./pages/auth/register/SignUp";
 import { StudentDashboard } from "./pages/studentDashboard/StudentDashboard";
 import { TutorDashboard } from "./pages/tutorDashboard/TutorDashboard";
 import TutorDropIn from "./pages/tutorDashboard/TutorDropIn.jsx";
+<<<<<<< Updated upstream
 import Profile from "./pages/profile";
+=======
+import Leaderboard from "./pages/tutorDashboard/Leaderboard";
+import Tutor_Feed from "./pages/studentDashboard/Tutor_Feed";
+>>>>>>> Stashed changes
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/auth/login" element={<Login />} />
+        <Route path="/" element={<Login />} />
         <Route path="/auth/signup" element={<SignUp />} />
         <Route path="/studentdashboard" element={<StudentDashboard />} />
         <Route path="/tutordashboard" element={<TutorDashboard />} />
+        <Route path="/tutorleaderboard" element={<Leaderboard/>}/>
+        <Route path="/tutorfeed" element={<Tutor_Feed/>}/>
         <Route path="/tutorprofile" element={<TutorDashboard />} />
         <Route path="/tutordropin" element={<TutorDropIn />} />
         <Route path="/profile" element={<Profile />} />
