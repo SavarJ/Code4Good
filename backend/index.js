7a1440fccb6116594bf74b97f5988824f378e7bd
Savar Jain - Home MBA <jainsavar@gmail.com>
2022-09-17 04:58:06 -0400

    Server

--- backend/index.js
+++ backend/index.js
@@ -1,2 +1,10 @@
-console.log('Hello from the other side');
+const express = require("express");
+const app = express();
 
+const cors = require("cors");
+app.use(cors());
+app.use(express.json());
+
+app.listen(5000, () => {
+  console.log("Server started on port 5000");
+});
